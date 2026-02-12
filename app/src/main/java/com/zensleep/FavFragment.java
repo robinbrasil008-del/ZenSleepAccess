b7d22b7271359773cd504a7a6286b57f6024866e
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-12 13:01:58 -0300

    Create FavFragment.java

---
+++ app/src/main/java/com/zensleep/FavFragment.java
@@ -0,0 +1,15 @@
+package com.zensleep;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FavFragment extends Fragment {
+
+    public FavFragment() {
+        super(R.layout.fragment_fav);
+    }
+}
