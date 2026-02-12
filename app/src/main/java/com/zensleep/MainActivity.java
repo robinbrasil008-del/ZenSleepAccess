06ac91d3a70fb9fd9621dbb2f420e9f4af46a87d
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-12 03:07:55 -0300

    Update MainActivity.java

--- app/src/main/java/com/zensleep/MainActivity.java
+++ app/src/main/java/com/zensleep/MainActivity.java
@@ -1,9 +1,6 @@
 package com.zensleep;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.btnTeste);
-
-        btn.setOnClickListener(v ->
-                Toast.makeText(this, "App funcionando!", Toast.LENGTH_SHORT).show()
-        );
+        setContentView(R.layout.activity_main); // ⚠️ ISSO É OBRIGATÓRIO
     }
 }
