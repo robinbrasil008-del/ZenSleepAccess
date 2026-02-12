8a62db3cf4705503582e661f189aa4665e1feeab
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-12 03:40:26 -0300

    Update MainActivity.java

--- app/src/main/java/com/zensleep/MainActivity.java
+++ app/src/main/java/com/zensleep/MainActivity.java
@@ -1,14 +1,29 @@
 package com.zensleep;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton playChuva;
+    ImageButton playMar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main); // ⚠️ ISSO É OBRIGATÓRIO
+        playChuva = findViewById(R.id.playChuva);
+        playMar = findViewById(R.id.playMar);
+
+        playChuva.setOnClickListener(v ->
+                Toast.makeText(this, "Chuva tocando 🌧", Toast.LENGTH_SHORT).show()
+        );
+
+        playMar.setOnClickListener(v ->
+                Toast.makeText(this, "Mar tocando 🌊", Toast.LENGTH_SHORT).show()
+        );
     }
 }
