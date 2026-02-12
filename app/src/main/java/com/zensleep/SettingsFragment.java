e39e5305e0c36e9efd685d6a97f7348c46301d97
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-12 13:02:32 -0300

    Create SettingsFragment.java

---
+++ app/src/main/java/com/zensleep/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.zensleep;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        super(R.layout.fragment_settings);
+    }
+}
