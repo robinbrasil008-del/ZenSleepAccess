f42ff25cf62ae7cb782ce66d653c9706c511354e
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-12 13:20:50 -0300

    Update FavFragment.java

--- app/src/main/java/com/zensleep/FavFragment.java
+++ app/src/main/java/com/zensleep/FavFragment.java
@@ -1,9 +1,8 @@
 package com.zensleep;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,5 +10,27 @@ public class FavFragment extends Fragment {
 
     public FavFragment() {
         super(R.layout.fragment_fav);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        TextView favList = view.findViewById(R.id.favList);
+
+        StringBuilder builder = new StringBuilder();
+
+        if (FavoritesManager.isFavorite(getContext(), "chuva")) {
+            builder.append("🌧 Chuva\n");
+        }
+
+        if (FavoritesManager.isFavorite(getContext(), "mar")) {
+            builder.append("🌊 Ondas do Mar\n");
+        }
+
+        if (builder.length() == 0) {
+            builder.append("Nenhum favorito ainda.");
+        }
+
+        favList.setText(builder.toString());
     }
 }
