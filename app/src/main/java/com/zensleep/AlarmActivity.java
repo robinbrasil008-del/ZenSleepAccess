d68f52fc44a0358be2c2a1c02cda3fa0655c2a85
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-13 06:25:56 -0300

    Update AlarmActivity.java

--- app/src/main/java/com/zensleep/AlarmActivity.java
+++ app/src/main/java/com/zensleep/AlarmActivity.java
@@ -1,14 +1,75 @@
 package com.zensleep;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class AlarmActivity extends AppCompatActivity {
+
+    private AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Button btnAddAlarm = findViewById(R.id.btnAddAlarm);
+
+        btnAddAlarm.setOnClickListener(v -> openTimePicker());
+    }
+
+    private void openTimePicker() {
+
+        Calendar now = Calendar.getInstance();
+
+        TimePickerDialog dialog = new TimePickerDialog(
+                this,
+                (view, hourOfDay, minute) -> scheduleAlarm(hourOfDay, minute),
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                true
+        );
+
+        dialog.show();
+    }
+
+    private void scheduleAlarm(int hour, int minute) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        if (calendar.before(Calendar.getInstance())) {
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        Intent intent = new Intent(this, AlarmReceiver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this,
+                100,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                calendar.getTimeInMillis(),
+                pendingIntent
+        );
+
+        Toast.makeText(this, "Alarme agendado!", Toast.LENGTH_SHORT).show();
     }
 }
