fef658822a2c1acd13440e19c25bc33fc5db2dec
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-13 06:15:26 -0300

    Create AlarmActivity.java

---
+++ app/src/main/java/com/zensleep/AlarmActivity.java
@@ -0,0 +1,14 @@
+package com.zensleep;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
+}
