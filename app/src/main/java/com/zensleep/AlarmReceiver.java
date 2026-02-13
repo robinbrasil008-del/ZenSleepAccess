9c64c6466c8fae2717ce17ddc1218b9eddb9ed1f
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-13 19:14:27 -0300

    Update AlarmReceiver.java

--- app/src/main/java/com/zensleep/AlarmReceiver.java
+++ app/src/main/java/com/zensleep/AlarmReceiver.java
@@ -1,67 +1,19 @@
 package com.zensleep;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
-
-    private static final String CHANNEL_ID = "alarm_channel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Intent fullScreenIntent = new Intent(context, AlarmRingingActivity.class);
-        fullScreenIntent.putExtra("alarm_id", intent.getIntExtra("alarm_id", -1));
-        fullScreenIntent.putExtra("alarm_label", intent.getStringExtra("alarm_label"));
+        Toast.makeText(context, "Receiver executado!", Toast.LENGTH_LONG).show();
 
-        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(
-                context,
-                0,
-                fullScreenIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
-        );
-
-        createNotificationChannel(context);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Alarme")
-                .setContentText("Toque para abrir")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-                .setFullScreenIntent(fullScreenPendingIntent, true)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(1001, builder.build());
-    }
-
-    private void createNotificationChannel(Context context) {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            NotificationChannel channel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Alarm Channel",
-                    NotificationManager.IMPORTANCE_HIGH
-            );
-
-            channel.setDescription("Canal para alarmes");
-
-            NotificationManager manager =
-                    context.getSystemService(NotificationManager.class);
-
-            if (manager != null) {
-                manager.createNotificationChannel(channel);
-            }
-        }
+        Intent i = new Intent(context, AlarmRingingActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 }
