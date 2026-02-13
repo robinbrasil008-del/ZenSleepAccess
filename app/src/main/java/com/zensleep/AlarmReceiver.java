91f9ee2d65ddc62c8c55a43bb0ddaf92c80e99d2
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-13 17:44:13 -0300

    Update AlarmReceiver.java

--- app/src/main/java/com/zensleep/AlarmReceiver.java
+++ app/src/main/java/com/zensleep/AlarmReceiver.java
@@ -3,20 +3,33 @@ package com.zensleep;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Intent i = new Intent(context, AlarmRingingActivity.class);
+        try {
 
-        i.putExtra("alarm_id", intent.getIntExtra("alarm_id", -1));
-        i.putExtra("alarm_label", intent.getStringExtra("alarm_label"));
+            int alarmId = intent.getIntExtra("alarm_id", -1);
+            String label = intent.getStringExtra("alarm_label");
 
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-                   Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            Intent ringingIntent = new Intent(context, AlarmRingingActivity.class);
+            ringingIntent.putExtra("alarm_id", alarmId);
+            ringingIntent.putExtra("alarm_label", label);
 
-        context.startActivity(i);
+            ringingIntent.addFlags(
+                    Intent.FLAG_ACTIVITY_NEW_TASK |
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP
+            );
+
+            context.startActivity(ringingIntent);
+
+        } catch (Exception e) {
+            Log.e("AlarmReceiver", "Erro ao iniciar AlarmRingingActivity", e);
+        }
     }
 }
