b158f3703becf15a033c338b2bc2239f4fafada7
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-15 05:05:57 -0300

    Update AlarmReceiver.java

--- app/src/main/java/com/zensleep/AlarmReceiver.java
+++ app/src/main/java/com/zensleep/AlarmReceiver.java
@@ -3,21 +3,24 @@ package com.zensleep;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import androidx.core.content.ContextCompat;
+import android.os.Build;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        int alarmId = intent.getIntExtra("alarm_id", -1);
+        int alarmId = intent.getIntExtra("alarm_id", 9999);
         String label = intent.getStringExtra("alarm_label");
 
         Intent serviceIntent = new Intent(context, AlarmService.class);
         serviceIntent.putExtra("alarm_id", alarmId);
         serviceIntent.putExtra("alarm_label", label);
 
-        ContextCompat.startForegroundService(context, serviceIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
     }
 }
