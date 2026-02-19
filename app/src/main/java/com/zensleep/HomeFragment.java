e81a175695b4d3aabf0f1334b8a63d7fe2776474
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-18 22:28:54 -0300

    Update HomeFragment.java

--- app/src/main/java/com/zensleep/HomeFragment.java
+++ app/src/main/java/com/zensleep/HomeFragment.java
@@ -251,7 +251,8 @@ public class HomeFragment extends Fragment {
                 @Override public void onStartTrackingTouch(SeekBar sb) {}
                 @Override public void onStopTrackingTouch(SeekBar sb) {}
             });
-        }
+         }
+      });
     }
 
     private void stopSingle(String key, ImageView button) {
