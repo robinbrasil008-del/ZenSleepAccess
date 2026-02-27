2b4199bb501110a45518a6f6e82c13e297898df8
robinbrasil008-del <robinbrasil008@gmail.com>
2026-02-27 02:10:03 -0300

    Update MainActivity.java

--- app/src/main/java/com/zensleep/MainActivity.java
+++ app/src/main/java/com/zensleep/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         );
 
         super.onCreate(savedInstanceState);
+        MobileAds.initialize(this);
         setContentView(R.layout.activity_main);
 
         // ==========================
