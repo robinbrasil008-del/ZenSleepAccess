8ce25ef479eb5a424c6195a1baeef1d561e4a4d0
robinbrasil008-del <robinbrasil008@gmail.com>
2026-04-27 17:34:11 -0300

    Set theme for MainActivity

--- app/src/main/java/com/zensleep/MainActivity.java
+++ app/src/main/java/com/zensleep/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        setTheme(R.style.Theme_ZenSleep);
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
