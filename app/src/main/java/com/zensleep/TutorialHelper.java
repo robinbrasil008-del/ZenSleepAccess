7ba24c161c0781a15596a4298bffe99ab9d6afc0
robinbrasil008-del <robinbrasil008@gmail.com>
2026-04-30 23:15:07 -0300

    Update TutorialHelper.java

--- app/src/main/java/com/zensleep/TutorialHelper.java
+++ app/src/main/java/com/zensleep/TutorialHelper.java
@@ -168,7 +168,7 @@ public class TutorialHelper {
                         sinalMais.setTextSize(45f); 
                         sinalMais.setTextColor(Color.WHITE);
                         sinalMais.setTypeface(null, Typeface.BOLD);
-                        sinalMais.setShadowLayer(10f, 0f, 0f, Color.parseColor("#CC000000"));
+                        sinalMais.setShadowLayer(10f, 0f, 0f, Color.parseColor("#008000"));
                         tutorialOverlay.addView(sinalMais);
                     }
                     sinalMais.setVisibility(View.VISIBLE);
