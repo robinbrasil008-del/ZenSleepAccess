c3ee7f6befc87c277e13b3433ef2977b99a899e8
robinbrasil008-del <robinbrasil008@gmail.com>
2026-06-01 21:07:38 -0300

    Correct dialog view setup in AlarmConfigActivity
    Fixed method call for setting dialog view.

--- app/src/main/java/com/zensleep/AlarmConfigActivity.java
+++ app/src/main/java/com/zensleep/AlarmConfigActivity.java
@@ -94,8 +94,8 @@ public class AlarmConfigActivity extends AppCompatActivity {
         // Usamos o LayoutInflater da própria Activity
         View dialogView = getLayoutInflater().inflate(R.layout.dialog_select_sound, null);
 
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(dialogView);
+        final Dialog dialog = new Dialog(this);
+        dialog.settView(dialogView);
 
         // 5. Puxamos os IDs diretamente da variável do dialog
         TextView option1 = dialog.findViewById(R.id.optionSound1);
